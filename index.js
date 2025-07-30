309fa929991eecd01eded1c16c29090fa9d9f6e6
dcryptau <dcrypt.au@gmail.com>
2025-07-30 11:13:57 +0530

    All files working

---
+++ index.js
@@ -0,0 +1,60 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// Models
+const User = require('./models/User');
+
+// Routes
+const userRoute = require('./routes/user');
+const additionalDetailsRoute = require('./routes/additionalDetails');
+const eventRoute = require('./routes/event');
+
+// Cloudinary
+const cloudinary = require('cloudinary');
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+});
+
+// Connect to MongoDB
+
+mongoose.connect(process.env.DB_URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Connection error:', err));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(cookieParser())
+app.use(cors());
+
+// CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+// Routes
+app.use('/', userRoute);
+app.use('/', additionalDetailsRoute);
+app.use('/', eventRoute);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ utils/mailsender.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      service:"gmail",
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    let info = await transporter.sendMail({
+      from: "Univent - Event Management Platform",
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
+
+    return info; // Return the result to be handled by the caller
+  } catch (error) {
+    console.log("Error in mailSender", error.message);
+    throw error; // Throw the error to be caught by the caller
+  }
+};
+
+module.exports = mailSender;
